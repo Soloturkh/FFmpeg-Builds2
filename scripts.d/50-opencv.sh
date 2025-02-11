d2e399a8484165a571bf4d61d91b946e35a25815
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2025-02-11 11:30:21 +0300

    Update 50-opencv.sh

--- scripts.d/50-opencv.sh
+++ scripts.d/50-opencv.sh
@@ -135,8 +135,8 @@ ffbuild_dockerbuild() {
         -DOPENCV_ENABLE_PKG_CONFIG=ON \
         -DOPENCV_GENERATE_PKGCONFIG=ON \
         -DOPENCV_PC_FILE_NAME=opencv4.pc \
-	-DCMAKE_INSTALL_LIBDIR="$FFBUILD_PREFIX/lib" \
-        -DCMAKE_INSTALL_PKGCONFIGDIR="$FFBUILD_PREFIX/lib/pkgconfig" \
+	-DCMAKE_INSTALL_LIBDIR=lib \
+        -DCMAKE_INSTALL_PKGCONFIGDIR=pkgconfig \
         -DOPENCV_ENABLE_NONFREE=ON \
         -DBUILD_EXAMPLES=OFF \
 		-DINSTALL_PYTHON_EXAMPLES=OFF \
@@ -162,7 +162,7 @@ ffbuild_dockerbuild() {
     while IFS= read -r pc_file; do
         echo "Bulundu: $pc_file -> $FFBUILD_PREFIX/lib/pkgconfig/"
 	cat "$pc_file"  # Dosya içeriğini ekrana yazdır
-        # cp -f "$pc_file" "$FFBUILD_PREFIX/lib/pkgconfig/"
+        cp -f "$pc_file" "$FFBUILD_PREFIX/lib/pkgconfig/"
     done <<< "$found_pc_files"
 }
 
@@ -175,7 +175,7 @@ ffbuild_unconfigure() {
 }
 
 ffbuild_cflags() {
-    echo -I$FFBUILD_PREFIX/include/opencv4 -I$FFBUILD_PREFIX/include
+    echo -I$FFBUILD_PREFIX/include/opencv4
 }
 
 ffbuild_ldflags() {
