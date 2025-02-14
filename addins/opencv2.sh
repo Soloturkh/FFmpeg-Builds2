00d2d430590ff0cce6361130cb279c97c1b676a4
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2025-02-14 10:14:50 +0300

    Update opencv2.sh

--- addins/opencv2.sh
+++ addins/opencv2.sh
@@ -1,1 +1,20 @@
-libopencv-dev
+#!/bin/bash
+set -euo pipefail
+
+DEBIAN_FRONTEND=noninteractive sudo apt-get update && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
+build-essential cmake git ninja-build pkg-config \
+wget yasm nasm libtool autoconf automake
+
+# OpenCV + contrib static build
+FF_CONFIGURE+=" --enable-libopencv"
+PKG_CONFIG_LIBDIR+=":/usr/local/share/pkgconfig:/usr/local/bin/pkgconfig"
+FF_CFLAGS+=" -I/usr/local/include/opencv4"
+FF_LDFLAGS+=" -L/usr/local/lib -lopencv_world"
+
+
+build() {
+  DEBIAN_FRONTEND=noninteractive apt install libopencv-dev
+}
+
+# Build komutunu çağır
+build
