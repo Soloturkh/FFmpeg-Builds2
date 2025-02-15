cc735ff2ea07c58ccdeff1e63a679953a0db505e
Soloturkh <127161170+Soloturkh@users.noreply.github.com>
2025-02-16 01:56:20 +0300

    Create 40-libopencv.sh

---
+++ scripts.d/40-libopencv.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+SCRIPT_REPO="https://github.com/opencv/opencv.git"
+OPENCV_VERSION="4.10.0"
+
+ffbuild_enabled() {
+    [[ $TARGET == linux* ]] && return 0
+    return 1
+}
+
+ffbuild_dockerdl() {
+	  # OpenCV core
+    git clone https://github.com/opencv/opencv.git --branch $OPENCV_VERSION --depth 1
+    mkdir -p opencv/build && cd opencv/build
+  
+    # OpenCV contrib
+    git clone https://github.com/opencv/opencv_contrib.git --branch $OPENCV_VERSION --depth 1 ../contrib
+}
+
+ffbuild_dockerbuild() {
+    if command -v nvidia-smi &> /dev/null; then
+    		echo "NVIDIA GPU algılandı, CUDA desteği ve TBB devre dışı bırakılıyor..."
+    		GPU_OPTIONS="-DWITH_CUDA=ON \
+    					  -DWITH_CUDNN=ON \
+    					  -DOPENCV_DNN_CUDA=ON \
+    					  -DCUDA_ARCH_BIN=ALL \
+    					  -DENABLE_FAST_MATH=ON \
+    					  -DCUDA_FAST_MATH=ON \
+    					  -DWITH_CUBLAS=ON \
+    					  -DBUILD_opencv_cudacodec=ON"
+    		TBB_OPTION="-DWITH_TBB=OFF"  # GPU mevcutsa TBB devre dışı
+    else
+    		echo "NVIDIA GPU algılanmadı, sadece CPU desteği etkinleştiriliyor..."
+    		GPU_OPTIONS="-DWITH_CUDA=OFF \
+    					  -DWITH_CUDNN=OFF \
+    					  -DOPENCV_DNN_CUDA=OFF \
+    					  -DWITH_CUBLAS=OFF \
+    					  -DBUILD_opencv_cudacodec=OFF"
+    		TBB_OPTION="-DWITH_TBB=ON"  # GPU yoksa TBB etkin
+    fi
+
+    cmake -G Ninja -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+        -DCMAKE_INSTALL_BINDIR="$FFBUILD_PREFIX"/bin -DCMAKE_INSTALL_LIBDIR="$FFBUILD_PREFIX"/lib \
+        -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
+        -DENABLE_PRECOMPILED_HEADERS=OFF \
+        -DBUILD_EXAMPLES=OFF \
+        -DBUILD_TESTS=OFF \
+        -DBUILD_PERF_TESTS=OFF \
+		    $GPU_OPTIONS \
+		    $TBB_OPTION \
+        -DWITH_OPENCL=OFF \
+        -DWITH_V4L=ON \
+		    -DWITH_FFMPEG=OFF \
+		    -DWITH_GSTREAMER=OFF \
+        -DWITH_MSMF=OFF \
+		    -DWITH_DSHOW=OFF \
+		    -DWITH_AVFOUNDATION=OFF \
+		    -DWITH_1394=OFF \
+        -DWITH_IPP=OFF \
+        -DWITH_PROTOBUF=OFF \
+        -DENABLE_CXX11=ON \
+        -DBUILD_PKG_CONFIG=ON \
+        -DOPENCV_GENERATE_PKGCONFIG=ON \
+        -DOPENCV_ENABLE_NONFREE=ON \
+        -DBUILD_EXAMPLES=OFF \
+		    -DINSTALL_PYTHON_EXAMPLES=OFF \
+		    -DINSTALL_C_EXAMPLES=OFF \
+		    -DBUILD_ZLIB=ON \
+		    -DBUILD_SHARED_LIBS=OFF \
+        ..
+
+    ninja -j$(nproc)
+    ninja install
+
+    cat >"$FFBUILD_PREFIX"/lib/pkgconfig/opencv.pc <<EOF
+prefix=$FFBUILD_PREFIX
+exec_prefix=\${prefix}
+libdir=\${prefix}/lib
+sharedlibdir=\${prefix}/lib
+includedir=\${prefix}/include/opencv4
+
+Name: OpenCV
+Description: Open Source Computer Vision Library
+Version: $OPENCV_VERSION
+
+Requires: 
+Libs: -L\${libdir} -L\${sharedlibdir} -lopencv_imgproc -lopencv_core -lstdc++
+Cflags: -I\${includedir}
+Libs.private: -lstdc++
+EOF
+}
+
+ffbuild_configure() {
+    echo --enable-libopencv
+}
+
+ffbuild_unconfigure() {
+    echo --disable-libopencv
+}
